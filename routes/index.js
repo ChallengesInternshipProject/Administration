dfa07c18357dc67ca8441fa1d03ef1bb9aae8a0f
Krasimir Velichkov <krasimirvelichkov@gmail.com>
2016-09-23 07:13:30 +0300

    test commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+var mongoose = require('mongoose')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+mongoose.connect('mongodb://192.168.137.89:27017/dareornot')
+  /* GET home page. */
+router.get('/', function (req, res, next) {
+  res.send('ok')
+})
 
-module.exports = router;
+module.exports = router
