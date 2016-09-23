ca2afd485534db688ca98469633e2311ebabb05f
Krasimir Velichkov <krasimirvelichkov@gmail.com>
2016-09-23 07:30:21 +0300

    Adding Dockerfile

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express')
 var router = express.Router()
 var mongoose = require('mongoose')
 
-mongoose.connect('mongodb://192.168.137.89:27017/dareornot')
-  /* GET home page. */
+// mongoose.connect('mongodb://192.168.137.89:27017/dareornot')
+//   /* GET home page. */
 router.get('/', function (req, res, next) {
   res.send('ok')
 })
